8c91bc4516d39f9d75d6e10cfa45f0ca87c25d78
aidaptivecom-pixel <aidaptive.com@gmail.com>
2026-01-13 18:09:27 -0300

    feat: Update animation exports and add global CSS animations

--- components/animations/index.ts
+++ components/animations/index.ts
@@ -4,6 +4,7 @@ export { default as AnimatedCard } from './AnimatedCard';
 export { default as AnimatedCounter } from './AnimatedCounter';
 export { default as AnimatedModal } from './AnimatedModal';
 export { default as AnimatedProgress } from './AnimatedProgress';
+export { default as AnimatedKPICard, AnimatedKPIGrid } from './AnimatedKPICard';
 export { AnimatedList, AnimatedListItem } from './AnimatedList';
 export { AnimatedTableBody, AnimatedTableRow } from './AnimatedTable';
 
